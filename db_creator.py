14912d1f11734afde958c0236b3a0a6e9f359c98
Daniel <kronkowski.daniel@gmail.com>
2023-07-02 18:55:13 +0200

    change database name

--- db_creator.py
+++ db_creator.py
@@ -16,7 +16,7 @@ conn.autocommit = True
 c = conn.cursor()
 
 try:
-    c.execute('CREATE DATABASE db_cs;')
+    c.execute('CREATE DATABASE db_cs_threading;')
 except errors.DuplicateDatabase:
     pass
 
