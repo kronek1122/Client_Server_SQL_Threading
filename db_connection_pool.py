4c2e328dfa36391840981650b1a40e5e8ceeca36
Daniel <kronkowski.daniel@gmail.com>
2023-08-24 20:07:31 +0200

    change type of ConnectionPool working, first step

--- db_connection_pool.py
+++ db_connection_pool.py
@@ -2,23 +2,23 @@ import threading
 import time
 import os
 from queue import Queue, Empty, Full
-from db import DatabaseManager
+import psycopg2
 from dotenv import load_dotenv
 
 class ConnectionPool:
-    def __init__(self, min_connections=5, max_connections=50):
+    def __init__(self, database, user, password, host):
         load_dotenv()
-        self.min_connections = min_connections
-        self.max_connections = max_connections
-        self.host = os.getenv('host')
-        self.port = os.getenv('port')
-        self.database = os.getenv('database')
-        self.user = os.getenv('user')
-        self.password = os.getenv('password')
-        self.semaphore = threading.Semaphore(max_connections)
-        self.connections = Queue(maxsize=max_connections)
+        self.min_connections = 4
+        self.max_connections = 50
+        self.host = host
+        self.database = database
+        self.user = user
+        self.password = password
+        self.connections = Queue(maxsize=self.max_connections)
+        self.semaphore = threading.Semaphore()
         self.start_time = time.time()
         self.connections_released = 0
+        self.active_connections = 0
         self.initialize_connections()
 
         self.connections_check = threading.Thread(target=self.connections_manager)
@@ -27,14 +27,13 @@ class ConnectionPool:
 
     def initialize_connections(self):
         for _ in range(self.min_connections):
-            connection = self.create_connection()
-            if connection:
-                self.connections.put(connection)
+            self.create_connection()
 
 
     def create_connection(self):
         try:
-            connection = DatabaseManager(self.database, self.user, self.password, self.host)
+            connection = psycopg2.connect(self.database, self.user, self.password, self.host)
+            self.connections.put(connection)
             return connection
         except Exception as exp:
             print("Error creating connection:", exp)
@@ -45,31 +44,38 @@ class ConnectionPool:
         with self.semaphore:
             try:
                 connection = self.connections.get(timeout=2)
+                self.active_connections +=1
             except Empty:
-                try:
-                    self.initialize_connections()
+                if self.connections.qsize() + self.active_connections < self.max_connections:
+                    self.create_connection()
                     connection = self.connections.get(timeout=2)
-                except Full:
-                    raise Exception('Connection pool limit reached')  
+                    self.active_connections +=1
+                else:
+                    while True:
+                        try:
+                            connection = self.connections.get(timeout=2)
+                            break
+                        except Empty:
+                            pass
             return connection
 
 
     def connections_manager(self):
         while True:
-            time.sleep(2)
-            with self.semaphore:
-                while self.connections.qsize()>self.min_connections:
-                    connection = self.connections.get()
-                    try:
-                        connection.close()
-                    except Exception:
-                        pass
+            while self.connections.qsize()>self.min_connections:
+                connection = self.connections.get()
+                try:
+                    connection.close()
+                except Exception as exp:
+                    print("Error:", exp)
 
             print(f"""
                 Time from start: {round(time.time() - self.start_time, 2)}
                 Realised connections: {self.connections_released}
-                Active connections: {self.connections.qsize()}
-""")
+                Active connections: {self.active_connections}
+                Connections in Queue: {self.connections.qsize()}
+                """)
+            time.sleep(3)
 
 
     def destroy_connections(self):
@@ -78,17 +84,19 @@ class ConnectionPool:
                 connection = self.connections.get()
                 try:
                     connection.close()
-                except Exception:
-                    pass
-                    
+                except Exception as exp:
+                    print("Error:", exp)
+
 
     def release_connection(self, connection):
         with self.semaphore:
             try:
                 self.connections.put(connection, timeout=2)
+                self.active_connections -=1
             except Full:
                 try:
                     connection.close()
-                except Exception:
-                    pass
+                except Exception as exp:
+                    print("Error:", exp)
             self.connections_released += 1
+
